ec7e6d394ef0758daef8e32c3c8256ef4782f744
mranjith <r.mathiyazhagan@dxc.com>
2026-01-14 19:06:26 +0530

    removed full customer things

--- src/routes/customer.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import authController from '../controllers/auth.controller';
-
-const router = Router();
-
-router.get('/last', authController.getLatestCustomer.bind(authController));
-
-export default router;
